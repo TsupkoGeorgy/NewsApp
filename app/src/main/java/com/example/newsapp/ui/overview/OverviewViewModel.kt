fea5dc678d8555769c6f94920b70a38553dc2d0a
Skaara9111 <skaara911@gmail.com>
2022-02-04 23:50:09 +0300

    Add OverviewFragment.kt and OverviewViewModel.kt

---
+++ app/src/main/java/com/example/newsapp/ui/overview/OverviewViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.newsapp.ui.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+
+class OverviewViewModel: ViewModel() {
+
+
+
+//    private val _navigateToSelectedProperty = MutableLiveData<Articles?>()
+//    val navigateToSelectedProperty: LiveData<Articles?>
+//        get() = _navigateToSelectedProperty
+
+    //Pager
+//    val flow: LiveData<PagingData<Articles>> = Pager(
+//        PagingConfig(pageSize = 5)
+//    ) {
+//
+//        EverythingNewsPagingSource(NewsApi.retrofitService, "sport", language.value!!)
+//    }.liveData.cachedIn(viewModelScope)
+
+}
