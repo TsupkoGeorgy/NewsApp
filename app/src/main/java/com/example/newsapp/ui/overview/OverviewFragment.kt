e3820dc0f5801b0aec9a15bc239c41e89a031b11
Skaara9111 <skaara911@gmail.com>
2022-02-06 01:29:15 +0300

    Change OverviewFragment.kt and OverviewViewModel.kt

--- app/src/main/java/com/example/newsapp/ui/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/overview/OverviewFragment.kt
@@ -1,27 +1,116 @@
 package com.example.newsapp.ui.overview
 
 import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.androidcentranewsapp.ui.overview.LoaderStateAdapter
+import com.example.androidcentranewsapp.ui.overview.PagingAdapter
 import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentOverviewBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 
 class OverviewFragment : Fragment() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
+    private val viewModel: OverviewViewModel by lazy {
+        ViewModelProvider(this).get(OverviewViewModel::class.java)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        val binding: FragmentOverviewBinding =
+            DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_overview,
+                container,
+                false
+            )
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        val pagingAdapter = PagingAdapter(PagingAdapter.OnClickListener {
+            if (it != null) {
+                viewModel.displayPropertyDetails(it)
+            }
+        })
+
+        viewModel.filterUpdate.observe(viewLifecycleOwner, Observer {
+            pagingAdapter.refresh()
+        })
+
+        binding.grid.adapter = pagingAdapter
+
+        binding.grid.adapter =
+            pagingAdapter.withLoadStateFooter(LoaderStateAdapter { pagingAdapter.retry() })
+
+
+        viewModel.flow.observe(viewLifecycleOwner, Observer {
+            viewLifecycleOwner.lifecycleScope.launch {
+                pagingAdapter.submitData(viewModel.flow.value!!)
+            }
+        })
+
+
+//        viewLifecycleOwner.lifecycleScope.launch {
+//            viewModel.flow.collectLatest {  pagingData ->
+//                pagingAdapter.submitData(pagingData)
+//            }
+//        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            pagingAdapter.loadStateFlow.collectLatest { loadState ->
+                loadState.refresh
+            }
+        }
+
+        viewModel.navigateToSelectedProperty.observe(viewLifecycleOwner, Observer {
+            if (null != it) {
+                this.findNavController()
+                    .navigate(OverviewFragmentDirections.actionOverviewFragmentToWebViewFragment(it.url))
+                viewModel.displayWebViewComplete()
+            }
+        })
+
+        binding.grid.layoutManager = GridLayoutManager(context, 1)
+
+
+        setHasOptionsMenu(true)
+        return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.overflow_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        viewModel.updateLanguage(
+            when (item.itemId) {
+                R.id.show_eu_responce -> "en"
+                R.id.show_ru_responce -> "ru"
+                else -> "en"
+            }
+
+        )
+        viewModel.updateFilterComplete()
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+
+    }
+
 
 }
--- app/src/main/java/com/example/newsapp/ui/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/overview/OverviewViewModel.kt
@@ -4,24 +4,56 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
+import androidx.paging.*
+import com.example.newsapp.data.network.EverythingNewsPagingSource
+import com.example.newsapp.data.network.NewsApi
+import com.example.newsapp.data.network.news.ArticlesNews
 
 class OverviewViewModel: ViewModel() {
 
 
 
-//    private val _navigateToSelectedProperty = MutableLiveData<Articles?>()
-//    val navigateToSelectedProperty: LiveData<Articles?>
-//        get() = _navigateToSelectedProperty
+    private val _filterUpdate = MutableLiveData<Boolean>()
+    val filterUpdate: LiveData<Boolean>
+        get() = _filterUpdate
 
-    //Pager
-//    val flow: LiveData<PagingData<Articles>> = Pager(
-//        PagingConfig(pageSize = 5)
-//    ) {
-//
-//        EverythingNewsPagingSource(NewsApi.retrofitService, "sport", language.value!!)
-//    }.liveData.cachedIn(viewModelScope)
+    private val _language = MutableLiveData<String>()
+    private val language: LiveData<String>
+        get() = _language
+
+    private val _navigateToSelectedProperty = MutableLiveData<ArticlesNews?>()
+    val navigateToSelectedProperty: LiveData<ArticlesNews?>
+        get() = _navigateToSelectedProperty
+
+    val flow: LiveData<PagingData<ArticlesNews>> = Pager(
+        PagingConfig(pageSize = 5)
+    ) {
+
+        EverythingNewsPagingSource(NewsApi.retrofitService, "sport", language.value!!)
+    }.liveData.cachedIn(viewModelScope)
+
+
+    fun displayPropertyDetails(ArticlesNews: ArticlesNews) {
+        _navigateToSelectedProperty.value = ArticlesNews
+    }
+
+    fun displayWebViewComplete() {
+        _navigateToSelectedProperty.value = null
+    }
+
+
+    init {
+        _language.value = "en"
+
+
+    }
+    fun updateLanguage(filter: String) {
+        _language.value = filter
+        _filterUpdate.value = true
+    }
+
+    fun updateFilterComplete() {
+        _filterUpdate.value = false
+    }
 
 }
